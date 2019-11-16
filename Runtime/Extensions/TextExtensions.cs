28ffbf80cc591fed2f3584e8fa40df9a66fe2108
yuyang <yuyang9119@outlook.com>
2019-11-16 17:17:59 +0800

    已添加        Runtime/Extensions/ReaderWriterExtensions.cs 已添加        Runtime/Extensions/ReaderWriterExtensions.cs.meta 已添加        Runtime/Extensions/TextExtensions.cs 已添加        Runtime/Extensions/TextExtensions.cs.meta 已添加        Runtime/Extensions/UnityExtensions.cs 已添加        Runtime/Extensions/UnityExtensions.cs.meta

---
+++ Runtime/Extensions/TextExtensions.cs
@@ -0,0 +1,34 @@
+﻿
+namespace UnityExtensions
+{
+    /// <summary>
+    /// 文本处理相关扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static int IndexOfNonWhiteSpace(this string text, int startIndex = 0)
+        {
+            for (; startIndex < text.Length; startIndex++)
+            {
+                if (!char.IsWhiteSpace(text, startIndex)) return startIndex;
+            }
+            return -1;
+        }
+
+        public static int LastIndexOfNonWhiteSpace(this string text, int startIndex)
+        {
+            for (; startIndex >= 0; startIndex--)
+            {
+                if (!char.IsWhiteSpace(text, startIndex)) return startIndex;
+            }
+            return -1;
+        }
+
+        public static int LastIndexOfNonWhiteSpace(this string text)
+        {
+            return LastIndexOfNonWhiteSpace(text, text.Length - 1);
+        }
+
+    } // class Extensions
+
+} // namespace UnityExtensions
