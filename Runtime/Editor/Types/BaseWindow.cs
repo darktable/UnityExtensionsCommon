492e144a03d4a93f075f02718fac4b65ae5f93b2
yuyang <yuyang9119@outlook.com>
2019-12-16 08:53:38 +0800

    modify file structure

--- Runtime/Editor/Types/BaseWindow.cs
+++ Runtime/Editor/Types/BaseWindow.cs
@@ -9,22 +9,14 @@ namespace UnityExtensions.Editor
     /// </summary>
     public abstract class BaseWindow : EditorWindow
     {
-        [System.NonSerialized] bool _visible;
-
-
-        public bool visible => _visible;
-
-
         protected virtual void OnEnable()
         {
-            _visible = true;
             Undo.undoRedoPerformed += OnUndoRedoPerformed;
         }
 
 
         protected virtual void OnDisable()
         {
-            _visible = false;
             Undo.undoRedoPerformed -= OnUndoRedoPerformed;
         }
 
@@ -32,12 +24,6 @@ namespace UnityExtensions.Editor
         protected virtual void OnUndoRedoPerformed()
         {
             Repaint();
-        }
-
-
-        public void RepaintIfVisible()
-        {
-            if (_visible) Repaint();
         }
 
     } // class BaseWindow
---
+++ Runtime/Editor/Types/SettingsWindow.cs
@@ -0,0 +1,82 @@
+﻿#if UNITY_EDITOR
+
+using System.IO;
+using UnityEditor;
+
+namespace UnityExtensions.Editor
+{
+    public class EditorSettings<T> where T : EditorSettings<T>, new()
+    {
+        static T _instance;
+        public static T instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                    EditorJsonUtility.FromJsonOverwrite(_instance.Load(), _instance);
+                }
+                return _instance;
+            }
+        }
+
+        internal void Serialize()
+        {
+            Save(EditorJsonUtility.ToJson(this, true));
+        }
+
+        protected virtual void Save(string data)
+        {
+            try
+            {
+                using (var stream = new FileStream($"ProjectSettings/{typeof(T).Name}", FileMode.Create, FileAccess.Write))
+                {
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        writer.Write(data);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        protected virtual string Load()
+        {
+            try
+            {
+                using (var stream = new FileStream($"ProjectSettings/{typeof(T).Name}", FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    /// <summary>
+    /// SettingsWindow<T>
+    /// </summary>
+    public abstract class SettingsWindow<T> : BaseWindow where T : EditorSettings<T>, new()
+    {
+        protected static T settings => EditorSettings<T>.instance;
+
+
+        protected virtual void OnLostFocus()
+        {
+            settings.Serialize();
+        }
+
+    } // class SettingsWindow<T>
+
+} // namespace UnityExtensions.Editor
+
+#endif // UNITY_EDITOR
