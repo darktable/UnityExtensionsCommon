b45b2323dc648cb69845f88847d35b79912a6cfe
yuyang9119 <yuyang9119@outlook.com>
2020-08-02 23:19:17 +0800

    add dynamic label

---
+++ Runtime/Attributes/DynamicLabelAttribute.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using System.Reflection;
+using UnityEditor;
+using UnityExtensions.Editor;
+#endif
+
+namespace UnityExtensions
+{
+    /// <summary>
+    /// Change the label text of a field
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public sealed class DynamicLabelAttribute : PropertyAttribute
+    {
+        public DynamicLabelAttribute(string labelProperty)
+        {
+#if UNITY_EDITOR
+            _labelProperty = labelProperty;
+#endif
+        }
+
+#if UNITY_EDITOR
+
+        string _labelProperty;
+        PropertyInfo _propertyInfo;
+
+        [CustomPropertyDrawer(typeof(DynamicLabelAttribute))]
+        class DynamicLabelDrawer : BasePropertyDrawer<DynamicLabelAttribute>
+        {
+            PropertyInfo propertyInfo
+            {
+                get
+                {
+                    if (attribute._labelProperty != null)
+                    {
+                        var prop = fieldInfo.DeclaringType.GetInstanceProperty(attribute._labelProperty);
+                        if (prop?.PropertyType == typeof(string) && prop.CanRead)
+                        {
+                            attribute._propertyInfo = prop;
+                        }
+                        attribute._labelProperty = null;
+                    }
+                    return attribute._propertyInfo;
+                }
+            }
+
+            public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+            {
+                object text = propertyInfo?.GetValue(property.GetParentObject(), null);
+                if (text != null) label.text = (string)text;
+                base.OnGUI(position, property, label);
+            }
+        }
+
+#endif // UNITY_EDITOR
+
+    }
+
+} // namespace UnityExtensions
