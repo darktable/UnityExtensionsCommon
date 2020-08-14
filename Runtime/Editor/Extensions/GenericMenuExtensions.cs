4329c2c8b4c4c940c88a2c5172c45d7c0118f9ff
yuyang9119 <yuyang9119@outlook.com>
2020-08-15 03:52:43 +0800

    add menu extension

---
+++ Runtime/Editor/Extensions/GenericMenuExtensions.cs
@@ -0,0 +1,41 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityExtensions.Editor
+{
+    /// <summary>
+    /// Extensions for GenericMenu.
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static void AddItem(this GenericMenu menu, GUIContent content, GenericMenu.MenuFunction func, bool disabled = false, bool on = false)
+        {
+            if (disabled)
+            {
+                menu.AddDisabledItem(content, on);
+            }
+            else
+            {
+                menu.AddItem(content, on, func);
+            }
+        }
+
+        public static void AddItem(this GenericMenu menu, GUIContent content, GenericMenu.MenuFunction2 func, object userData, bool disabled = false, bool on = false)
+        {
+            if (disabled)
+            {
+                menu.AddDisabledItem(content, on);
+            }
+            else
+            {
+                menu.AddItem(content, on, func, userData);
+            }
+        }
+
+    } // class Extensions
+
+} // namespace UnityExtensions.Editor
+
+#endif // UNITY_EDITOR
